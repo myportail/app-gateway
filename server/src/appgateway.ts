ccb1e79baed415c1bf48915e512aaabf8c2ad363
dannyt <cobreti_@hotmail.com>
2019-04-21 22:13:17 -0400

    fixed config and deployment

--- server/src/appgateway.ts
+++ server/src/appgateway.ts
@@ -16,12 +16,27 @@ declare var process : {
 //     [key: string]: any;
 // }
 
+function ReadConfig(configFilename: string): AppConfig | null{
+    try {
+        const appConfig = new AppConfig();
+        const fileContent = fs.readFileSync(`configs/${configFilename}.json`, 'utf8');
+        if (fileContent) {
+            appConfig.initFromJSON(JSON.parse(fileContent));
+        }
+
+        return appConfig;
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return null;
+}
+
 const configName = process.env.ENVIRONMENT_NAME;
 const configPlatform = (configName) ? `.${configName}` : '';
 const configFilename = `config${configPlatform}`;
-
-const appConfig = new AppConfig();
-appConfig.initFromJSON(JSON.parse(fs.readFileSync(`configs/${configFilename}.json`, 'utf8')));
+const appConfig = ReadConfig(configFilename);
 
 const app: express.Application = express();
 const port: number = process.env.PORT || 8000;
@@ -34,7 +49,12 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
 app.get('/config', (req: any, res: { send: (arg0: string) => void; }) => {
-   res.send(JSON.stringify(appConfig.toJSON()));
+    if (appConfig) {
+        res.send(JSON.stringify(appConfig.toJSON()));
+    }
+    else {
+        res.send(`no config found at ${configFilename}`);
+    }
 });
 
 app.use(new Proxy('/api/auth/',
